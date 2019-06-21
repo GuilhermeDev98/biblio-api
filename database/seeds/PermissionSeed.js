1dd50a39f0498fa2b34230cd58605a464398d1b6
Guilherme Santos <guilhermedev@hotmail.com>
2019-06-21 11:11:04 -0300

    run migrations and seeds

--- database/seeds/PermissionSeed.js
+++ database/seeds/PermissionSeed.js
@@ -1,32 +1,32 @@
 const Permission = require("../../src/models/Permission");
 
-// const models = ["role", "permission", "user", "book", "loan"];
-// const methods = [
-//   "index",
-//   "create",
-//   "store",
-//   "show",
-//   "edit",
-//   "update",
-//   "delete"
-// ];
+const models = ["role", "permission", "user", "book", "loan"];
+const methods = [
+  "index",
+  "create",
+  "store",
+  "show",
+  "edit",
+  "update",
+  "delete"
+];
 const roleId = 2;
 
-// models.map(async model => {
-//   methods.map(async method => {
-//     await Permission.create({
-//       name: `${method}_${model}`,
-//       description: `${method} ${model}`,
-//       roleId
-//     });
-//   });
-// });
+models.map(async model => {
+  methods.map(async method => {
+    await Permission.create({
+      name: `${method}_${model}`,
+      description: `${method} ${model}`,
+      roleId
+    });
+  });
+});
 
-// Permission.create({
-//   name: "giveback_loan",
-//   description: "giveback loan",
-//   roleId
-// });
+Permission.create({
+  name: "giveback_loan",
+  description: "giveback loan",
+  roleId
+});
 
 Permission.create({
   name: "toextend_loan",

df28c7d17f38e3267e0d461fd47a58d61a81bd98
Guilherme Santos <guilhermedev@hotmail.com>
2019-06-21 10:25:07 -0300

    add procfile

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,25 @@
 require("../../start/index");
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "mariadb",
+
+if (process.env.ENV == "development") {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      dialect: process.env.DB_DIALECT,
+      dialectOptions: {
+        connectTimeout: 1000
+      }
+    }
+  );
+} else if (process.env.ENV == "production") {
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: process.env.DB_DIALECT,
     dialectOptions: {
       connectTimeout: 1000
     }
-  }
-);
+  });
+}
 
 module.exports = sequelize;

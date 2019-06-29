ffbfc0199ceed60a9dd543ee5031e5bd6e60ba1f
Guilherme Santos <guilhermedev@hotmail.com>
2019-06-29 14:51:01 -0300

    add route files

--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
+
 app.use(require("./router"));
 
 app.listen(port, () => {
